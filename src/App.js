ef00dc6fe7d91e2d0e2ed7a2b4cc5fe858d6723a
Sachin-kr21 <sachin210302@gmail.com>
2023-04-07 19:08:16 +0530

    separating pending and done section

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import TaskCard from "./TaskCard";
 
 function App() {
   return (
-    <div>
-      <h1>Smarter Tasks</h1>
+    
+    <div class="grid grid-cols-6">
+    <div class="col-start-2 col-span-4">
+    <h1>Smarter Tasks</h1>
+     <div class="grid grid-cols-2 divide-x">
       <div className="pending">
         <h1>Pending</h1>
-        < TaskCard title="Studying" dueDate="01 Jan" assignee="Sachin"/>
-        < TaskCard title="Boxing" dueDate="02 Jan" assignee="Sachin"/>
-        < TaskCard title="Flexing" dueDate="03 Jan" assignee="Sachin"/>
+        < TaskCard title="Studying" dueDate="01 Jan" assigneeName="Sachin"/>
+        < TaskCard title="Boxing" dueDate="02 Jan" assigneeName="Ruby"/>
+        < TaskCard title="Flexing" dueDate="03 Jan" assigneeName="Rohit"/>
       </div>
       <div className="done">
         <h1>Done</h1>
-          < TaskCard title="Playing" completed="04 Jan" assignee="Sachin"/>
-          < TaskCard title="Listening" completed="05 Jan" assignee="Sachin"/>
+          < TaskCard title="Playing" completedAtDate="04 Jan" assigneeName="Shivani"/>
+          < TaskCard title="Listening" completedAtDate="05 Jan" assigneeName="Shareen"/>
+          < TaskCard title="Singing" completedAtDate="06 Jan" assigneeName="Shamshadh"/>
       </div>
+    </div>
+    </div>
     </div>
   );
 }
--- src/TaskCard.js
+++ src/TaskCard.js
@@ -2,13 +2,13 @@ import './TaskCard.css'
 
 const TaskCard = (props) => {
   console.log(props)
-  
+
     if (props.dueDate){
         return (
         <div className='TaskItem'>
         <h2 className="text-xl font-bold">{props.title}</h2>
         <p>Due on: {props.dueDate}</p>
-        <p>Assignee: {props.assignee}</p>
+        <p>Assignee: {props.assigneeName}</p>
       </div>
         )
     }
@@ -16,8 +16,8 @@ const TaskCard = (props) => {
         return (
             <div className='TaskItem'>
             <h2 className="text-xl font-bold">{props.title}</h2>
-            <p>Completed on: {props.completed}</p>
-            <p>Assignee: {props.completed}</p>
+            <p>Completed on: {props.completedAtDate}</p>
+            <p>Assignee: {props.assigneeName}</p>
           </div>
             )
     }
