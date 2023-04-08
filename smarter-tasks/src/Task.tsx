783b5bad8e39e276413f74825d62211214a0812d
Sachin-kr21 <sachin210302@gmail.com>
2023-04-09 02:59:58 +0530

    adding taskitem class

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -11,7 +11,7 @@ interface TaskProp {
 class Task extends React.Component<TaskProp> {
     render() {
         return (
-          <div className="TaskItem shadow-md border border-slate-100" >
+          <div className="TaskItem" >
             <h2 className="text-base font-bold my-1">{this.props.title}</h2>
             <p className="text-sm text-slate-500">{this.props.dueDate}</p>
             <p className="text-sm text-slate-500">
