5dfb8e267832e6d003c46581348d0e4ed3379551
Sachin-kr21 <sachin210302@gmail.com>
2023-04-09 02:24:58 +0530

    Wd-301 l-3

---
+++ smarter-tasks/src/TaskForm.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { TaskItem } from "./types";
+import TaskList from "./TaskList";
+
+
+interface TaskFormProps {
+    addTask: (task: TaskItem) => void;
+}
+interface TaskFormState {
+    title: string;
+    dueDate: string;
+    description: string;
+}
+class TaskForm extends React.Component<TaskFormProps,TaskFormState>{
+    constructor(props : TaskFormProps){
+        super(props);
+        this.state = {
+            title : "",
+            dueDate: "",
+            description: ""
+            
+        }
+    }
+
+    addTask: React.FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault();
+        const newTask = {
+          title: this.state.title,
+          dueDate: this.state.dueDate,
+          description: this.state.description
+        }
+        try{
+            this.props.addTask(newTask);
+        }
+        catch(error){
+            console.log(error);
+        }
+        
+        this.setState({ title: "" });
+        this.setState({ dueDate: "" });
+        this.setState({ description: "" });
+      };
+    
+    // inputRef = React.createRef<HTMLInputElement>();
+    titleChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        console.log(`${event.target.value}`);
+        this.setState({ title: event.target.value });
+    };
+
+    dueDateChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        console.log(`${event.target.value}`);
+        this.setState({ dueDate: event.target.value });
+    };
+
+    descriptionChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        console.log(`${event.target.value}`);
+        this.setState({ description: event.target.value });
+    };
+
+    render(): React.ReactNode {
+        return (
+            // <div>Task form</div>
+            <form onSubmit={this.addTask}>
+                <input type="text" value={this.state.title} onChange={this.titleChanged}/>
+                <input type="text" id="todoDueDate" value={this.state.dueDate} onChange={this.dueDateChanged} required/>
+                <input type="text" id="todoDescription" value={this.state.description} onChange={this.descriptionChanged} required/>
+                <button type="submit" id="addTaskButton">Add item</button>
+            </form>
+        )
+    }
+}
+
+export default TaskForm;
---
+++ smarter-tasks/src/TaskList.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Task from "./Task";
+import { TaskItem } from "./types";
+
+interface Props {
+    tasks : TaskItem[];
+}
+
+// interface TaskItem {
+//     title: string
+// }
+
+interface State {
+    // tasks: TaskItem[];
+}
+
+
+class TaskList extends React.Component<Props, State> {
+    // constructor(props: Props) {
+    //   super(props);
+    //   this.state = {
+    //     tasks: [{ title: "Pay rent" }, { title: "Submit assignment" }],
+    //   };
+    // }
+    // componentDidMount() {
+    //     const tasks = [{title : "Pay rent"},{title: "Submit assignment"}];
+    //     this.setState((state , props) => ({
+    //         tasks,
+    //     }));
+    // }
+    render() {
+      return this.props.tasks.map((task , idx) => <div ><Task key={idx} title={task.title} dueDate={task.dueDate} description={task.description} /></div>) ;
+      
+    }
+  }
+
+  
+
+
+
+export default TaskList;
