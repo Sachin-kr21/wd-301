e2e5bb9890678d20c686c054291e8287b5a8ca6d
Sachin-kr21 <sachin210302@gmail.com>
2023-08-09 11:37:53 +0530

    notfound removal:

--- smarter-tasks/src/routes/index.tsx
+++ smarter-tasks/src/routes/index.tsx
@@ -7,7 +7,7 @@ import Signup from "../pages/signup"
 import Projects from "../pages/projects"
 import Members from "../pages/members"
 import Logout from "../pages/logout";
-import NotFound from "../pages/NotFound";
+// import NotFound from "../pages/NotFound";
 
 const router = createBrowserRouter([
     { path: "/", element: <Navigate to="/account/projects" replace /> },
@@ -44,10 +44,10 @@ const router = createBrowserRouter([
       },
     ],
   },
-  {
-    path: "*",
-    element: (<NotFound/>)
-  }
+  // {
+  //   path: "*",
+  //   element: (<NotFound/>)
+  // }
 ]);
 
 export default router;
