dd841875afd4c5a8cd862860ee192fb1345ca2f2
Sachin-kr21 <sachin210302@gmail.com>
2023-08-09 15:28:17 +0530

    Not found file addition

--- smarter-tasks/src/pages/signin/SigninForm.tsx
+++ smarter-tasks/src/pages/signin/SigninForm.tsx
@@ -74,7 +74,7 @@ const SigninForm: React.FC = () => {
         {...register('password', { required: true })}
         className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
       </div>
-      <button type="submit" className="w-full bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-gray mt-4">Sign In</button>
+      <button type="submit" id="submit" className="w-full bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-gray mt-4">Sign In</button>
     </form>
   );
 };
