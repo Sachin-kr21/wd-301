09ed5f39ddd3304f893cce32f7e0b4742e795b98
Sachin-kr21 <sachin210302@gmail.com>
2023-08-21 23:39:11 +0530

    L9 submission

--- smarter-tasks/dev-dist/sw.js
+++ smarter-tasks/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m9pj0r0flno"
+    "revision": "0.68b2plvb1qo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- smarter-tasks/src/pages/members/index.tsx
+++ smarter-tasks/src/pages/members/index.tsx
@@ -1,27 +1,55 @@
+
+
 import React, { Suspense } from "react";
-import MemberList from "./MemberList";
+const MemberList = React.lazy(() => import("./MemberList"));
 import NewMember from "./NewMember";
 import ErrorBoundary from "../../components/ErrorBoundary";
 
-const Projects = () => {
+const Members = () => {
   return (
     <>
       <div className="flex justify-between">
-        <h2 className="text-2xl font-medium tracking-tight">
+        <h2 className="text-2xl font-medium tracking-tight text-slate-700">
           Members
-          </h2>
+        </h2>
         <NewMember />
       </div>
       <ErrorBoundary>
-
-      <Suspense fallback={<div className="suspense-loading">Loading...</div>}>
-
-      <MemberList />
-      </Suspense>
+        <Suspense fallback={<div className="suspense-loading">Loading...</div>}>
+          <MemberList />
+        </Suspense>
       </ErrorBoundary>
-
     </>
-  )
+  );
 };
 
-export default Projects;
+
+export default Members;
+
+// import React, { Suspense } from "react";
+// import MemberList from "./MemberList";
+// import NewMember from "./NewMember";
+// import ErrorBoundary from "../../components/ErrorBoundary";
+
+// const Members = () => {
+//   return (
+//     <>
+//       <div className="flex justify-between">
+//         <h2 className="text-2xl font-medium tracking-tight">
+//           Members
+//           </h2>
+//         <NewMember />
+//       </div>
+//       <ErrorBoundary>
+
+//       <Suspense fallback={<div className="suspense-loading">Loading...</div>}>
+
+//       <MemberList />
+//       </Suspense>
+//       </ErrorBoundary>
+
+//     </>
+//   )
+// };
+
+// export default Members;
